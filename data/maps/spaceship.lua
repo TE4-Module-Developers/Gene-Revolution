d2f05e5a20513eb24c2c2b0b2ab6ee465247c147
ReenenLaurie <reenen@bieblebom.com>
2012-01-19 14:40:25 +0200

    Add empty tiled map, and a spaceship tiled map.  As well as the output lua map.
    Signed-off-by: ReenenLaurie <reenen@bieblebom.com>

---
+++ data/maps/spaceship.lua
@@ -0,0 +1,64 @@
+
+-- defineTile section
+defineTile("")
+defineTile("#", "HARDWALL")
+defineTile("4")
+defineTile("1")
+defineTile("2")
+
+-- addSpot section
+
+-- addZone section
+
+-- ASCII map section
+return [[
+
+
+
+
+
+
+#####
+####
+####
+##
+####
+#1#
+##
+####
+##
+#4#
+######
+########
+########
+####44####
+######
+####
+#22#
+####
+#22#
+##
+#22#
+##
+#22#
+#44#
+#22#
+##
+#22#
+##
+#22#
+##
+#22#
+##
+#242#
+##
+#22#
+##
+#22#
+##
+#22#
+##
+#22#
+##
+#22#
+##]]
